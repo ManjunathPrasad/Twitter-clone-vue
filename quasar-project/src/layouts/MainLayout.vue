b7282c3ddbe79313f4c4358602608218fc25044a
Code Monkey <manjunathprasad99@gmail.com>
2023-10-09 15:08:01 +0000

    Added first left, right and middle page

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -1,42 +1,141 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="lHr RpR fFf">
+    <q-header bordered class="bg-white text-black">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <!-- <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" /> -->
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title class="text-weight-bold"> Home </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
       show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      :width="250"
       bordered
     >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+      <q-icon class="q-pa-md" name="home" size="md" />
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+      <q-list>
+        <q-item to="/" clickable v-ripple exact>
+          <q-item-section avatar>
+            <q-icon class="q-pa-md" size="md" name="home" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple exact>
+          <q-item-section avatar>
+            <q-icon class="q-pa-md" size="md" name="search" />
+          </q-item-section>
+          <q-item-section class="text-h6 text-weight-bold"
+            >Search</q-item-section
+          >
+        </q-item>
+
+        <q-item to="/src/pages/PageNotifications.vue" clickable v-ripple exact>
+          <q-item-section avatar>
+            <q-icon class="q-pa-md" size="md" name="notifications" />
+          </q-item-section>
+          <q-item-section class="text-h6 text-weight-bold"
+            >Notifications</q-item-section
+          >
+        </q-item>
+
+        <q-item clickable v-ripple exact>
+          <q-item-section avatar>
+            <q-icon class="q-pa-md" size="md" name="mail_outline" />
+          </q-item-section>
+          <q-item-section class="text-h6 text-weight-bold"
+            >Messages</q-item-section
+          >
+        </q-item>
+
+        <q-item clickable v-ripple exact>
+          <q-item-section avatar>
+            <q-icon class="q-pa-md" size="md" name="list_alt" />
+          </q-item-section>
+          <q-item-section class="text-h6 text-weight-bold"
+            >Lists</q-item-section
+          >
+        </q-item>
+
+        <q-item clickable v-ripple exact>
+          <q-item-section avatar>
+            <q-icon class="q-pa-md" size="md" name="people" />
+          </q-item-section>
+          <q-item-section class="text-h6 text-weight-bold">
+            Communities
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple exact>
+          <q-item-section avatar>
+            <q-icon class="q-pa-md" size="md" name="account_circle" />
+          </q-item-section>
+          <q-item-section class="text-h6 text-weight-bold"
+            >Profile</q-item-section
+          >
+        </q-item>
+
+        <q-item clickable v-ripple exact>
+          <q-item-section avatar>
+            <q-icon class="q-pa-md" size="md" name="settings" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold"
+            >Settings</q-item-section
+          >
+        </q-item>
       </q-list>
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="right" side="right" :width="389" bordered>
+      <q-input
+        placeholder="Search Twitter"
+        class="q-ma-md"
+        outlined
+        rounded
+        dense
+      >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
+      <!-- -----------------------tabs------------------------ -->
+      <div class="q-pa-md">
+        <q-card class="my-card" flat bordered>
+          <q-card-section>
+            <div class="text-h6">Our Changing Planet</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+
+          <q-tabs v-model="tab" class="text-teal">
+            <q-tab label="Tab one" name="one" />
+            <q-tab label="Tab two" name="two" />
+          </q-tabs>
+
+          <q-separator />
+
+          <q-tab-panels v-model="tab" animated>
+            <q-tab-panel name="one">
+              The QCard component is a great way to display important pieces of
+              grouped content.
+            </q-tab-panel>
+
+            <q-tab-panel name="two">
+              With so much content to display at once, and often so little
+              screen real-estate, Cards have fast become the design pattern of
+              choice for many companies, including the likes of Google and
+              Twitter.
+            </q-tab-panel>
+          </q-tab-panels>
+        </q-card>
+      </div>
+      <!-- ----------------------------Tabs--------------------------------------- -->
     </q-drawer>
 
     <q-page-container>
@@ -46,71 +145,24 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { ref } from "vue";
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
-export default defineComponent({
-  name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
+export default {
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const rightDrawerOpen = ref(false);
 
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer() {
+        rightDrawerOpen.value = !rightDrawerOpen.value;
+      },
+    };
+  },
+};
 </script>
